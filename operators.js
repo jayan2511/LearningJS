1cefa9c34be22972f666b0b4dd864f1044d6d1ce
jayan2511 <jayan2511@gmail.com>
2023-01-05 01:39:01 +0530

    Adding code for Operators in JS

---
+++ operators.js
@@ -0,0 +1,36 @@
+// + Operator is used to concatenate strings and add numbers
+"aloha" + "!"
+250+250+500
+
+// - is used for subtract
+// * is used to multiply
+// / is used to divide
+
+1300-300
+500*4
+60/4
+
+// To use multiple operations, we can group using parenthesis
+((4 + 1 ) * 2 * 4) + 2
+
+// Precedence of operations = BODMAS
+
+// Comparison and Equality Operators
+// Returns true or false
+// >
+// <
+// >= 
+// <=
+// === equals
+// !== not equal
+
+// Using two equals signs is different than three equals signs
+1 == '1' // will return true
+1 === '1' // will return false
+
+
+// More operators:
+// %
+// || OR
+// && AND
+// ++ increment
